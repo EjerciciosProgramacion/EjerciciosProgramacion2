d1d14e30ced0c9677b32da0d569abbe955ac5f9d
luisaholguin <luisa-ro1992@hotmail.com>
2014-07-04 17:07:46 -0500

    Ejercicio13

--- src/ejerciciosprogramacion/Ejercicio13.java
+++ src/ejerciciosprogramacion/Ejercicio13.java
@@ -6,10 +6,38 @@
 
 package ejerciciosprogramacion;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @authora : Luisa Holguin
  */
 public class Ejercicio13 {
-
+   
+public static void main(String[] args) {
+    int n=0; int menor=0; int posicion =0;
+        n=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor del entero n"));
+        int[] arreglo1=new int[n];
+        int[] arregloauxiliar=new int[n];
+        for (int i = 0; i < n; i++) {
+            arreglo1 [i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese valores al vector"));
+        }
+         
+         for (int i = 0; i < n; i++) {
+             if (arreglo1[i] < 0) {
+                arregloauxiliar[posicion]= arreglo1[i];
+                posicion++;
+             }
+         }
+         for (int i = 0; i < n; i++) {
+            if (arreglo1[i] >= 0) {
+                arregloauxiliar[posicion]= arreglo1[i];
+                posicion++;
+             }
+        }
+         for (int i :arregloauxiliar) {
+        System.out.printf("%d ",i);
+    }
 }
+}
+

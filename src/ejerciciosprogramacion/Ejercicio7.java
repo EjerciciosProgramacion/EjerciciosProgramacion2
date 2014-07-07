8a5deba8fdef89213bebc60577715dbd7035a658
luisaholguin <luisa-ro1992@hotmail.com>
2014-07-07 09:09:36 -0500

    Ejercicio7

--- src/ejerciciosprogramacion/Ejercicio7.java
+++ src/ejerciciosprogramacion/Ejercicio7.java
@@ -6,10 +6,49 @@
 
 package ejerciciosprogramacion;
 
+import javax.swing.JOptionPane;
+import java.util.Scanner;
 /**
  *
- * @author Pc
+ * @authora : Luisa Holguin
  */
 public class Ejercicio7 {
-    
+     public static void main(String[] args) 
+     { 
+      int bandera=0;
+      String Luisa= " ";
+      int l = 0;
+      int numero=0,c=0;
+      JOptionPane.showMessageDialog(null," EJERCICIO 7 " , " Ejercicios de Programacion ", JOptionPane.INFORMATION_MESSAGE);
+      while (bandera==0) {
+        int codigo;
+        char x;
+        String res="";
+        String Cadena1, aux;
+        Cadena1=JOptionPane.showInputDialog("Ingrese su Frase");
+        int longitud1 = Cadena1.length();    
+        char [] frase1= Cadena1.toCharArray();    
+        for (int i = 0; i < longitud1; i++)
+        {
+            if (Cadena1.codePointAt(i)>=65 && Cadena1.codePointAt(i)<=90)
+            {
+                codigo = Cadena1.codePointAt(i);
+                codigo =codigo+32;
+                x=(char)codigo; 
+                res=res+x;                    
+            }
+            else                  
+            {
+                res=res+frase1[i];                    
+            }          
+        }
+        JOptionPane.showMessageDialog(null,res);
+        Luisa = JOptionPane.showInputDialog("Desea continuar ingresando de nuevo otros valores SI=0 , NO=1 ");
+        l = Integer.parseInt(Luisa);
+        if (l==1)  
+            bandera=1;                        
+        }       
+        JOptionPane.showMessageDialog(null," Fin del ejercicio 7 ");  
+    }
 }
+
